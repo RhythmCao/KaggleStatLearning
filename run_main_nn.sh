47025a7709eec3e896e3ca0715303b67a14d7bf6
RhythmCao <ruishengcao@gmail.com>
2018-12-11 16:23:14 +0800

    revise main.py to main_nn.py

--- run_main_nn.sh
+++ run_main_nn.sh
@@ -8,8 +8,8 @@ l2=1e-3
 dropout=0.5
 batchSize=64
 max_norm=5
-max_epoch=64
+max_epoch=100
 deviceId=0
 python main_nn.py --experiment $exp --model $model --nonlinear $nonlinear \
-    --affine_layers 512 128 --lr $lr --l2 $l2 --dropout $dropout --batchSize $batchSize --max_epoch $max_epoch --max_norm $max_norm \
+    --affine_layers 1024 256 64 --lr $lr --l2 $l2 --dropout $dropout --batchSize $batchSize --max_epoch $max_epoch --max_norm $max_norm \
     --optim $optim --deviceId $deviceId
