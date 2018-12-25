c2993d568590784b51dc7f081a355a082a03c96e
RhythmCao <ruishengcao@gmail.com>
2018-12-25 17:44:37 +0800

    retry on new dataset

--- run_main_nn.sh
+++ run_main_nn.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 exp=exp
-model=fnn # cnn
-nonlinear=relu
+model=fnn # cnn, fnn
+nonlinear=tanh # relu, sigmoid, tanh
 lr=0.001
 optim=adam
 l2=1e-3
-dropout=0.5
+dropout=0.5 # 0
 batchSize=64
 testbatchSize=64
 max_norm=5
@@ -13,6 +13,6 @@ max_epoch=50
 deviceId=0
 # --channel 512 128 32 --kernel_size 5 3 3 --maxpool_kernel_size 4 2 2 
 python main_nn.py --experiment $exp --model $model --nonlinear $nonlinear \
-    --affine_layers 1024 256 64\
+    --affine_layers 1024 256 64 \
     --lr $lr --l2 $l2 --dropout $dropout --batchSize $batchSize --max_epoch $max_epoch --max_norm $max_norm \
     --optim $optim --deviceId $deviceId --test_batchSize $testbatchSize #--batchnorm
--- run_main_stat.sh
+++ run_main_stat.sh
@@ -1,24 +1,48 @@
 #!/bin/bash
 exp=exp
-model=svm # lda
-type=svc # nusvc, linearsvc, lda, qda
 split=0
 
-gamma=scale # atuo
-C=1
-nu=0.5
-kernel=rbf # linear, poly, sigmoid
-decision_function_shape=ovr # ovo, crammer_singer
-degree=3
-coef0=0.0
-penalty=l2 # l1
-loss=squared_hinge # hinge
+# SVM or LDA Model
+model=svm # svm, lda
+type=nusvc # svc, nusvc, linearsvc, lda, qda
 
-reg_param=0.5
-solver=lsqr # svd, lsqr, eigen
-shrinkage=0.5 # float
-
-python main_stat.py --experiment $exp --model $model --type $type --split_ratio $split \
-            --kernel $kernel --gamma $gamma --C $C --nu $nu --decision_function_shape $decision_function_shape \
-            --degree $degree --coef0 $coef0 --penalty $penalty --loss $loss --dual
-            # --reg_param $shrinkage --solver $solver --shrinkage $shrinkage
+if [ $type == 'svc' ] ;then
+    C=1
+    kernel=rbf # rbf, linear, poly, sigmoid
+    degree=3 # degree of the poly kernel function
+    gamma=auto # auto, scale
+    coef0=0.0 # only significant in poly and sigmoid
+    decision_function_shape=ovr # ovo, ovr
+    python main_stat.py --experiment $exp --model $model --type $type --split_ratio $split \
+                --kernel $kernel --gamma $gamma --C $C --decision_function_shape $decision_function_shape \
+                --degree $degree --coef0 $coef0 
+elif [ $type == 'nusvc' ] ;then
+    nu=0.5 # upper bound on fraction of training errors and lower bound of fractions upport vectors
+    kernel=rbf # rbf, linear, poly, sigmoid
+    degree=3 # degree of the poly kernel function
+    gamma=scale # auto, scale
+    coef0=0.0 # only significant in poly and sigmoid
+    decision_function_shape=ovr # ovo, ovr
+    python main_stat.py --experiment $exp --model $model --type $type --split_ratio $split \
+                --kernel $kernel --gamma $gamma --nu $nu --decision_function_shape $decision_function_shape \
+                --degree $degree --coef0 $coef0 
+elif [ $type == 'linearsvc' ] ;then
+    C=1
+    loss=hinge # hinge, squared_hinge
+    penalty=l2 # l1, l2
+    decision_function_shape=ovr # ovr, crammer_singer
+    python main_stat.py --experiment $exp --model $model --type $type --split_ratio $split \
+                --C $C --decision_function_shape $decision_function_shape \
+                --penalty $penalty --loss $loss
+elif [ $type == 'lda' ] ;then
+    solver=eigen # svd, lsqr, eigen
+    shrinkage=0.5 # float
+    python main_stat.py --experiment $exp --model $model --type $type --split_ratio $split \
+                --solver $solver --shrinkage $shrinkage
+elif [ $type == 'qda' ] ;then
+    reg_param=0.5
+    python main_stat.py --experiment $exp --model $model --type $type --split_ratio $split \
+                --reg_param $reg_param
+else
+    echo "Unknown model type ... ..."
+fi
